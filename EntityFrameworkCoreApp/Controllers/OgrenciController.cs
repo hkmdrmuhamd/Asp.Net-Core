29137a8f1af1ef6450baa5b456aae85ab781002c
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-03-12 14:59:08 +0300

    Kayıtlı Öğrenciler Listesi

--- EntityFrameworkCoreApp/Controllers/OgrenciController.cs
+++ EntityFrameworkCoreApp/Controllers/OgrenciController.cs
@@ -1,6 +1,6 @@
 ﻿using EntityFrameworkApp.Data;
 using Microsoft.AspNetCore.Mvc;
-using System.Runtime.InteropServices;
+using Microsoft.EntityFrameworkCore;
 
 namespace EntityFrameworkCoreApp.Controllers
 {
@@ -13,17 +13,24 @@ namespace EntityFrameworkCoreApp.Controllers
             _context = context; //Bu kullanım ile DataContext sınıfına ait tüm entity'lere erşim sağlayabiliriz.
         }
 
+        public async Task<IActionResult> Index()
+        {
+            var ogrenciler = await _context.Ogrenciler.ToListAsync();
+            return View(ogrenciler);
+        }
+
         public IActionResult Create()
         {
             return View();
         }
+
 
         [HttpPost]
         public async Task<IActionResult> Create(Ogrenci model) //Database işlemleri genellikle async (asenkron) olur
         {
             _context.Add(model);
             await _context.SaveChangesAsync(); //Yapılan değişikler asenkron olarak yapılır ve kaydedilir
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index");
         }
     }
 }
--- EntityFrameworkCoreApp/Data/Ogrenci.cs
+++ EntityFrameworkCoreApp/Data/Ogrenci.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace EntityFrameworkApp.Data
@@ -5,10 +7,13 @@ namespace EntityFrameworkApp.Data
     public class Ogrenci
     {
         [Key]
+        [DisplayName("Öğrenci Id'si")]
         public int Id { get; set; } /*Bu id değeri primary key olmak zorundadır. Fakat eğer Id olarak belirlersek veya
             class adı Id => OgrenciId şeklinde belirlersek asp.net [Key] şeklinde belirtsek de belirmesek de bunun bir primary key olduğunu anlar 
             fakat eğer özel bir isim vermek istiyorsak: mesela OgrenciKimlik gibi o zaman kolonumuzun üstüne [Key] şeklinde belirtmemiz gereklidir.*/
+        [DisplayName("Öğrencinin Adı")]
         public string? Ad { get; set; }
+        [DisplayName("Öğrencinin Soyadı")]
         public string? Soyad { get; set; }
         public string? Email { get; set; }
         public string? Telefon { get; set; }
