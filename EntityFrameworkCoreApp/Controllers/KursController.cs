1b971e0c40a90ce999173ccaa03c0bcd8c6b6069
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-03-13 11:17:32 +0300

    Kurs Ekleme ve Kayıtlı Kursları Listeleme

---
+++ EntityFrameworkCoreApp/Controllers/KursController.cs
@@ -0,0 +1,35 @@
+﻿using EntityFrameworkApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCoreApp.Controllers
+{
+    public class KursController : Controller
+    {
+        private readonly DataContext _context;
+
+        public KursController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var kurs = await _context.Kurslar.ToListAsync();
+            return View(kurs);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Kurs model)
+        {
+            _context.Add(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- EntityFrameworkCoreApp/Data/Kurs.cs
+++ EntityFrameworkCoreApp/Data/Kurs.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel;
+
 namespace EntityFrameworkApp.Data
 {
     public class Kurs
     {
+        [DisplayName("Kurs Id")]
         public int KursId { get; set; }
+        [DisplayName("Kurs Adý")]
         public string? Baslik { get; set; }
     }
 }
