00456405df5bbcccbfbfaf81282a1daca23e92fa
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-03-13 11:54:19 +0300

    Kurs Güncelleme

--- EntityFrameworkCoreApp/Controllers/KursController.cs
+++ EntityFrameworkCoreApp/Controllers/KursController.cs
@@ -31,5 +31,52 @@ namespace EntityFrameworkCoreApp.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var kurs = await _context.Kurslar.FindAsync(id);
+            if (kurs == null)
+            {
+                return NotFound();
+            }
+            return View(kurs);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Kurs kurs)
+        {
+            if (id != kurs.KursId)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(kurs);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if(!_context.Kurslar.Any(k => k.KursId == id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                    
+                }
+                return RedirectToAction("Index"); 
+            }
+            return View(kurs);
+        }
     }
 }
